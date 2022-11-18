a44489901d2d1ac715ef435d0f66448dec2f6041
Ledvance-Wayne <w.li@ledvance.com>
2022-11-18 16:39:08 +0800

    fix issue activity not shows up.(somehow)

--- WayneAlgorithm/app/src/main/java/com/wayne/algorithm/activities/MainActivity.kt
+++ WayneAlgorithm/app/src/main/java/com/wayne/algorithm/activities/MainActivity.kt
@@ -1,8 +1,24 @@
 package com.wayne.algorithm.activities
 
+import android.content.pm.ActivityInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.ComposeView
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
+import com.wayne.LockScreenOrientation
 import com.wayne.algorithm.R
+import com.wayne.algorithm.ktxs.tryFold
+import com.wayne.algorithm.ktxs.tryReduce
 import com.wayne.algorithm.ktxs.wayneLogd
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -10,7 +26,39 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        wayneLogd("1st activity")
-        setContentView(R.layout.activity_main)
+        wayneLogd("it's 1st activity")
+        setContentView(ComposeView(this).apply {
+            layoutParams = ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
+            setContent {
+                LockScreenOrientation(orientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT)
+                val systemUiController = rememberSystemUiController()
+                SideEffect {
+                    systemUiController.setSystemBarsColor(Color.Transparent)
+                }
+                MaterialTheme {
+//                    CompositionLocalProvider(
+//                        LocalThemeColors provides colors,
+//                        LocalSystemUiController provides systemUiController,
+//                        content = content
+//                    )
+                    AppContent()
+                }
+            }
+        })
+    }
+}
+
+@Composable
+fun AppContent(){
+    LaunchedEffect(true){
+        val (foldRes, reduceRes) = listOf(1000, 100, 10, 1).let {
+            arrayOf(it.tryFold(), it.tryReduce())
+        }
+        wayneLogd(" foldRes -> $foldRes    reduceRes -> $reduceRes")
+    }
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .background(Color.Red)) {
+
     }
 }
