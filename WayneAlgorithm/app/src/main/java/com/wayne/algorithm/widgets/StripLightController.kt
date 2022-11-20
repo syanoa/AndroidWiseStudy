af140777e3abe134d13bf86287e0d2398b8e403e
wayne <w.li@ledvance.com>
2022-11-20 16:53:01 +0800

    initially add a custom view for rendering UI controller for strip light.

---
+++ WayneAlgorithm/app/src/main/java/com/wayne/algorithm/widgets/StripLightController.kt
@@ -0,0 +1,22 @@
+package com.wayne.algorithm.widgets
+
+import android.content.Context
+import android.graphics.Canvas
+import android.util.AttributeSet
+import android.view.View
+
+class StripLightController(ctx:Context, attrs: AttributeSet):View(ctx, attrs) {
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+//        setMeasuredDimension()
+    }
+
+    override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
+        super.onLayout(changed, left, top, right, bottom)
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+    }
+}
