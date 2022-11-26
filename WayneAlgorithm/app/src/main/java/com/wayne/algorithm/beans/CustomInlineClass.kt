358b372e83cf2260db8aa2507c96e84ba27c51dd
wayne <w.li@ledvance.com>
2022-11-26 17:05:51 +0800

    1. learning about inline class.

---
+++ WayneAlgorithm/app/src/main/java/com/wayne/algorithm/beans/CustomInlineClass.kt
@@ -0,0 +1,15 @@
+package com.wayne.algorithm.beans
+
+@JvmInline  //<- which replaces the inline modifier
+value class CustomInlineClass(val name:String) {
+    // cannot have default accessor because Kotlin would generate "Backing field" to it which is not allowed by inline class
+    val shitCount:Int
+      get() = name.length
+
+}
+
+inline class CustomInlineClassDeprecated(val name:String){
+    // cannot have default accessor because Kotlin would generate "Backing field" to it which is not allowed by inline class
+    val shitCount:Int
+        get() = name.length
+}
