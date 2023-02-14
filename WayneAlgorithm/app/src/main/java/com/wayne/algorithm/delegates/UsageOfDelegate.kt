429b28c8cb96012ee1842072d9bd6c9185f2b8db
Ledvance-Wayne <w.li@ledvance.com>
2023-02-14 10:49:47 +0800

    add restrict for generic T.

--- WayneAlgorithm/app/src/main/java/com/wayne/algorithm/delegates/UsageOfDelegate.kt
+++ WayneAlgorithm/app/src/main/java/com/wayne/algorithm/delegates/UsageOfDelegate.kt
@@ -10,14 +10,14 @@ sealed interface SuperDao
  class FamilyDao: SuperDao
  class UserDao:SuperDao
  class DeviceDao:SuperDao
-class ImagineDelegate<out T>{
+class ImagineDelegate<T>{
 }
-operator inline fun<reified T>  ImagineDelegate<T>.getValue(thisRef: Any?, property: KProperty<*>): T {
+inline operator fun<reified T:SuperDao>  ImagineDelegate<T>.getValue(thisRef: Any?, property: KProperty<*>): T {
     return when(T::class.java){
         FamilyDao::class.java-> FamilyDao() as T
         UserDao::class.java-> UserDao() as T
         DeviceDao::class.java->DeviceDao() as T
-        else -> DeviceDao() as T
+        else ->DeviceDao() as T
     }
 }
 fun <T>delegateFun():ImagineDelegate<T>{
